853a122b7d994482fd77f63bd1988bae66552b54
Orkun Demirci <orkundemircit147@gmail.com>
2023-10-08 12:10:37 +0300

    feat - Added applicationsetting object

---
+++ NorthwindAPI/Profiles/ProductProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NorthwindAPI.Core.Entities.Concrete;
+using NorthwindAPI.Models.Concrete.ProductModels;
+
+namespace NorthwindAPI.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            //CreateMap<Product, GetProductModel>().ForMember(p => p.ProductId, gp => gp.MapFrom(x => x.ProductId));
+            //CreateMap<Product, GetProductModel>().IncludeMembers(x => x.ProductId);
+            CreateMap<Product, GetProductVM>();
+            CreateMap<GetProductVM, Product>();
+            CreateMap<Product, AddProductVM>();
+            CreateMap<AddProductVM, Product>();
+            CreateMap<Product, UpdateProductVM>();
+            CreateMap<UpdateProductVM, Product>();
+            CreateMap<Product, DeleteProductVM>();
+            CreateMap<DeleteProductVM, Product>();
+        }
+    }
+}
